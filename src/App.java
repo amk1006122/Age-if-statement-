f9d9046a3094f1dfa79a63a8e51f323a25a29d22
amk1006122 <amk1006122@student.hamk.fi>
2024-09-08 20:04:09 +0300

    change variable value and if condition
    I changed the value of variable from 12 to 37. And changed the if condition.

--- src/App.java
+++ src/App.java
@@ -2,8 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
 
 
-        int age = 12;
-        if(age>0 && age<18)
+        int age = 37;
+        if(age>0 && age<20)
         {
             System.out.println("you are underage so, you can not drive a car.");
         }
