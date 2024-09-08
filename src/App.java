f03134a01db9dbf91aa015b0f7a717fd9e447b6d
amk1006122 <amk1006122@student.hamk.fi>
2024-09-08 20:00:57 +0300

    Changed the output line
    I added the decision in output line weather user can drive or not.

--- src/App.java
+++ src/App.java
@@ -5,11 +5,11 @@ public class App {
         int age = 12;
         if(age>0 && age<18)
         {
-            System.out.println("you are underage");
+            System.out.println("you are underage so, you can not drive a car.");
         }
         else
         {
-            System.out.println("you are adult");
+            System.out.println("you are adult so, you can drive a car.");
         }
         System.out.println(" ");
 
